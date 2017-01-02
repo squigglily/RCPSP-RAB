fb44ec783303d06f70dc533b692f8fe4c454bc4d
Rachel Broadbear <rbroadbear@gmail.com>
2017-01-01 19:43:44 -0600

    WHEEE

--- RCPSP.py
+++ RCPSP.py
@@ -1,14 +1,42 @@
 def main():
     openfile()
+    # to get raw printout, print(repr(<string>))
 
 def openfile():
     file = input("Please type the filename (including path) for the project:")
     f = open(file,"r")
     raw_data = f.read()
-    if raw_data[0].isnumeric():
-        print("Number")
-    elif raw_data[0] == "R":
-        print("Text")
-    else:
-        print("Error")
 
+    if raw_data[0].isnumeric() and raw_data[raw_data.find("\n") + 1].isnumeric():
+        print("Converting RCP file to appropriate format...")
+        convert_rcp(raw_data)
+    elif (raw_data[0:9] == "Resources" and raw_data[find_nth(raw_data,"\n",4) +
+            1:find_nth(raw_data,"\n",4) + 6] == "Tasks"):
+        print(raw_data)
+    else:
+        print("Error - the selected file appears to be in the wrong format.")
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+
+    return start
+
+def convert_rcp(raw_data):
+    import re
+    raw_data = re.sub(r' {1,}', ',',raw_data)
+    num_resources = int(raw_data[find_nth(raw_data,",",1) + 1:
+        find_nth(raw_data,",",2)])
+    resource_string = ""
+
+    for i in range(1,num_resources + 1):
+        resource_string = resource_string + "R" + str(i) + ","
+
+    raw_data = "Resources,\n" + resource_string + raw_data[raw_data.find("\n"):
+        find_nth(raw_data,"\n",2)] + "\n\nTasks,\nDuration," + resource_string \
+        + "NumSuccessors,Successors,\n" + raw_data[find_nth(raw_data,"\n",2) + 1:]
+    print(repr(raw_data))
+    print(raw_data)
