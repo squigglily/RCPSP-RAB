fb3b266047ceb2f3d521f95f3321de54e6a41bd0
Rachel Broadbear <rbroadbear@gmail.com>
2017-01-02 18:43:30 -0600

    WHEE

--- RCPSP.py
+++ RCPSP.py
@@ -12,7 +12,9 @@ def openfile():
         convert_rcp(raw_data)
     elif (raw_data[0:9] == "Resources" and raw_data[find_nth(raw_data,"\n",4) +
             1:find_nth(raw_data,"\n",4) + 6] == "Tasks"):
-        print(raw_data)
+        print("File is in appropriate format!")
+        csv_data = import_csv(file)
+        make_dictionary(csv_data)
     else:
         print("Error - the selected file appears to be in the wrong format.")
 
@@ -27,6 +29,7 @@ def find_nth(haystack, needle, n):
 
 def convert_rcp(raw_data):
     import re
+    import csv
     raw_data = re.sub(r' {1,}', ',',raw_data)
     num_resources = int(raw_data[find_nth(raw_data,",",1) + 1:
         find_nth(raw_data,",",2)])
@@ -38,5 +41,54 @@ def convert_rcp(raw_data):
     raw_data = "Resources,\n" + resource_string + raw_data[raw_data.find("\n"):
         find_nth(raw_data,"\n",2)] + "\n\nTasks,\nDuration," + resource_string \
         + "NumSuccessors,Successors,\n" + raw_data[find_nth(raw_data,"\n",2) + 1:]
-    print(repr(raw_data))
-    print(raw_data)
+    csv_data = list(csv.reader(raw_data.split("\n"), delimiter =","))
+    make_dictionary(csv_data)
+
+def import_csv(file):
+    import csv
+    csv_data = list(csv.reader(open(file, newline =""),delimiter=",", quotechar="|"))
+    return csv_data
+
+def make_dictionary(csv_data):
+    import csv
+    resource_list = strip_blanks([val for sublist in csv_data[1:2] for val in sublist])
+    resources = strip_blanks([val for sublist in csv_data[2:3] for val in sublist])
+    resource_dictionary = {}
+
+    if len(resource_list) != len(resources):
+        print("There is a problem with the resource list.  Please verify input data and try again.")
+        quit()
+    else:
+        for i in range(1,len(resource_list)+1):
+            resource_dictionary[i] = {}
+            resource_dictionary[i]["resource_name"] = resource_list[i-1]
+            resource_dictionary[i]["capacity"] = resources[i-1]
+
+    task_headers = strip_blanks([val for sublist in csv_data[5:6] for val in sublist])
+    task_data = csv_data[6:]
+    task_dictionary = []
+
+    for i in range(0,len(task_data)):
+        mini_task_data = task_data[i]
+        mini_task_data = strip_blanks(mini_task_data)
+
+        if len(mini_task_data) > len(task_headers)-1:
+            mini_task_data[len(task_headers)-1] = mini_task_data[len(task_headers)-1:]
+        elif len(mini_task_data) == len(task_headers)-1:
+            mini_task_data.append([''])
+        else:
+            Print("There is a problem with the task data.  Please verify input data and try again.")
+            quit()
+
+        mini_task_data[len(task_headers):] = ''
+        mini_task_data = strip_blanks(mini_task_data)
+        task_dictionary.append(mini_task_data)
+
+    #task_dictionary = dict(zip(task_headers,task_dictionary))
+    #print(dictionary)
+    print(task_dictionary)
+
+
+def strip_blanks(input_array):
+    output_array = [i for i in input_array if i !=""]
+    return(output_array)
